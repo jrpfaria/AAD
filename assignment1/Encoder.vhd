4f7141835e00632406cf517dc164d0b7f8c23955
danielsilva12UA <104782036+danielsilva12UA@users.noreply.github.com>
2023-11-10 01:26:20 +0000

    simplified encoder

--- assignment1/Encoder.vhd
+++ assignment1/Encoder.vhd
@@ -1,43 +1,25 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
-LIBRARY simpleLogic;
-USE simpleLogic.all;
-
 LIBRARY storeDev;
 USE storeDev.all;
 
 LIBRARY BuildingBlock1;
 USE BuildingBlock1.all;
 
-LIBRARY shiftRegister8;
-USE shiftRegister8.all;
-
 ENTITY Encoder IS
     PORT (
         clk, sIn, nGRst: IN  STD_LOGIC;
-        dOut:            OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
-		cont:			 OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
-		statO:			 OUT STD_LOGIC_VECTOR (5 DOWNTO 0);
-        bbIO:            OUT STD_LOGIC_VECTOR (8 DOWNTO 0);
-        bbOO:            OUT STD_LOGIC_VECTOR (7 DOWNTO 0)
+        dOut:            OUT STD_LOGIC_VECTOR (7 DOWNTO 0)
     );
 END Encoder;
 
 ARCHITECTURE structure OF Encoder IS
-    SIGNAL ffo: STD_LOGIC;
+    SIGNAL pr9In: STD_LOGIC_VECTOR (8 DOWNTO 0);
     SIGNAL iNSet, iNRst, clkO: STD_LOGIC;
     SIGNAL bbI: STD_LOGIC_VECTOR (8 DOWNTO 0);
     SIGNAL bbO: STD_LOGIC_VECTOR (7 DOWNTO 0);
     SIGNAL stat:  STD_LOGIC_VECTOR (5 DOWNTO 0);
-    COMPONENT shiftRegister8
-        PORT (
-            clk, nSet, nRst, load: IN STD_LOGIC;
-            serialIn:    IN  STD_LOGIC;
-            parallelIn:  IN  STD_LOGIC_VECTOR (7 DOWNTO 0);
-            parallelOut: OUT STD_LOGIC_VECTOR (7 DOWNTO 0)
-        );
-    END COMPONENT;
     COMPONENT BuildingBlock1
         PORT (
             dIn:  IN  STD_LOGIC_VECTOR (8 DOWNTO 0);
@@ -59,6 +41,14 @@ ARCHITECTURE structure OF Encoder IS
             Q:    OUT STD_LOGIC_VECTOR (7 DOWNTO 0)
         );
     END COMPONENT;
+    COMPONENT parReg_9bit
+        PORT (
+            nRst: IN  STD_LOGIC;
+            clk:  IN  STD_LOGIC;
+            D:    IN  STD_LOGIC_VECTOR (8 DOWNTO 0);
+            Q:    OUT STD_LOGIC_VECTOR (8 DOWNTO 0)
+        );
+    END COMPONENT;
     COMPONENT control
         PORT (
             nGRst: IN  STD_LOGIC;
@@ -69,25 +59,11 @@ ARCHITECTURE structure OF Encoder IS
             clkO:  OUT STD_LOGIC
         );
     END COMPONENT;
-    COMPONENT flipFlopDPET
-		PORT (
-			clk, D		: IN STD_LOGIC;
-			nSet, nRst 	: IN STD_LOGIC;
-			Q, nQ			: OUT STD_LOGIC
-		);
-	END COMPONENT;
     BEGIN
-        ff: flipFlopDPET PORT MAP (clk, sIn, '1', nGRst, ffO, open);
-		sr: shiftRegister8 PORT MAP (clk, iNRst, iNRst, '1', '1', bbO, bbI(8 DOWNTO 1));
-		bbI(0) <= ffO;
-        bbIO <= bbI;
-        bbOO <= bbO;
+        pr9In <= bbO & sIn;
+        pr9: parReg_9bit PORT MAP(iNRst, clk, pr9In, bbI);
 		bb1: BuildingBlock1 PORT MAP (bbI, bbO);
-		pr8: parReg_8bit PORT MAP(INSet, clkO, bbO, dOut);
+		pr8: parReg_8bit PORT MAP(iNSet, clkO, bbO, dOut);
 		bc:  binCounter_6bit PORT MAP (iNRst, clk, stat);
 		con: control  PORT MAP (nGRst, clk, stat, iNRst, iNSet, clkO);
-		statO <= stat;
-		cont(0) <= iNRst;
-		cont(1) <= iNSet;
-		cont(2) <= clkO;
 END structure;
--- assignment1/storeDev.vhd
+++ assignment1/storeDev.vhd
@@ -127,3 +127,66 @@ BEGIN
   ff4: flipFlopDPET PORT MAP (clk, D(4), nSet, '1', Q(4));
   ff5: flipFlopDPET PORT MAP (clk, D(5), nSet, '1', Q(5));
 END structure;
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY parReg_8bit IS
+	PORT (
+		nSet: IN STD_LOGIC;
+		clk: IN STD_LOGIC;
+		D: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+		Q: OUT STD_LOGIC_VECTOR (7 DOWNTO 0)
+	);
+END parReg_8bit;
+
+ARCHITECTURE structure OF parReg_8bit IS
+	COMPONENT flipFlopDPET
+		PORT (
+			clk, D:     IN  STD_LOGIC;
+			nSet, nRst: IN  STD_LOGIC;
+			Q, nQ:      OUT STD_LOGIC
+		);
+	END COMPONENT;
+	BEGIN
+		ff0: flipFlopDPET PORT MAP (clk, D(0), nSet, '1', Q(0));
+		ff1: flipFlopDPET PORT MAP (clk, D(1), nSet, '1', Q(1));
+		ff2: flipFlopDPET PORT MAP (clk, D(2), nSet, '1', Q(2));
+		ff3: flipFlopDPET PORT MAP (clk, D(3), nSet, '1', Q(3));
+		ff4: flipFlopDPET PORT MAP (clk, D(4), nSet, '1', Q(4));
+		ff5: flipFlopDPET PORT MAP (clk, D(5), nSet, '1', Q(5));
+		ff6: flipFlopDPET PORT MAP (clk, D(6), nSet, '1', Q(6));
+		ff7: flipFlopDPET PORT MAP (clk, D(7), nSet, '1', Q(7));
+END structure;
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY parReg_9bit IS
+	PORT (
+		nRst: IN STD_LOGIC;
+		clk: IN STD_LOGIC;
+		D: IN STD_LOGIC_VECTOR (8 DOWNTO 0);
+		Q: OUT STD_LOGIC_VECTOR (8 DOWNTO 0)
+	);
+END parReg_9bit;
+
+ARCHITECTURE structure OF parReg_9bit IS
+	COMPONENT flipFlopDPET
+		PORT (
+			clk, D:     IN  STD_LOGIC;
+			nSet, nRst: IN  STD_LOGIC;
+			Q, nQ:      OUT STD_LOGIC
+		);
+	END COMPONENT;
+	BEGIN
+		ff0: flipFlopDPET PORT MAP (clk, D(0), '1', nRst, Q(0));
+		ff1: flipFlopDPET PORT MAP (clk, D(1), '1', nRst, Q(1));
+		ff2: flipFlopDPET PORT MAP (clk, D(2), '1', nRst, Q(2));
+		ff3: flipFlopDPET PORT MAP (clk, D(3), '1', nRst, Q(3));
+		ff4: flipFlopDPET PORT MAP (clk, D(4), '1', nRst, Q(4));
+		ff5: flipFlopDPET PORT MAP (clk, D(5), '1', nRst, Q(5));
+		ff6: flipFlopDPET PORT MAP (clk, D(6), '1', nRst, Q(6));
+		ff7: flipFlopDPET PORT MAP (clk, D(7), '1', nRst, Q(7));
+    ff8: flipFlopDPET PORT MAP (clk, D(8), '1', nRst, Q(8));
+END structure;
