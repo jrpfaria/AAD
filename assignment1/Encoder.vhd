a8928b300ba5ca331223d8b244318f65a7f72ca9
danielsilva12UA <104782036+danielsilva12UA@users.noreply.github.com>
2023-11-10 16:08:09 +0000

    encoder fixed for real

--- assignment1/Encoder.vhd
+++ assignment1/Encoder.vhd
@@ -10,13 +10,17 @@ USE BuildingBlock1.all;
 ENTITY Encoder IS
     PORT (
         clk, sIn, nGRst: IN  STD_LOGIC;
-        dOut:            OUT STD_LOGIC_VECTOR (7 DOWNTO 0)
+        iOut:            OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
+        clk1OO:           OUT STD_LOGIC;
+        dOut:            OUT STD_LOGIC_VECTOR (23 DOWNTO 0)
     );
 END Encoder;
 
 ARCHITECTURE structure OF Encoder IS
     SIGNAL pr9In: STD_LOGIC_VECTOR (8 DOWNTO 0);
-    SIGNAL iNSet, iNRst, clkO: STD_LOGIC;
+    SIGNAL pr24In: STD_LOGIC_VECTOR (23 DOWNTO 0);
+    SIGNAL srO: STD_LOGIC_VECTOR (15 DOWNTO 0);
+    SIGNAL iNSet, iNRst, clkO, clk1O: STD_LOGIC;
     SIGNAL bbI: STD_LOGIC_VECTOR (8 DOWNTO 0);
     SIGNAL bbO: STD_LOGIC_VECTOR (7 DOWNTO 0);
     SIGNAL stat:  STD_LOGIC_VECTOR (4 DOWNTO 0);
@@ -33,14 +37,6 @@ ARCHITECTURE structure OF Encoder IS
             c:    OUT STD_LOGIC_VECTOR (4 DOWNTO 0)
         );
     END COMPONENT;
-    COMPONENT parReg_8bit
-        PORT (
-            nSet: IN  STD_LOGIC;
-            clk:  IN  STD_LOGIC;
-            D:    IN  STD_LOGIC_VECTOR (7 DOWNTO 0);
-            Q:    OUT STD_LOGIC_VECTOR (7 DOWNTO 0)
-        );
-    END COMPONENT;
     COMPONENT parReg_9bit
         PORT (
             nRst: IN  STD_LOGIC;
@@ -49,6 +45,22 @@ ARCHITECTURE structure OF Encoder IS
             Q:    OUT STD_LOGIC_VECTOR (8 DOWNTO 0)
         );
     END COMPONENT;
+    COMPONENT parReg_24bit
+        PORT (
+            nSet: IN  STD_LOGIC;
+            clk:  IN  STD_LOGIC;
+            D:    IN  STD_LOGIC_VECTOR (23 DOWNTO 0);
+            Q:    OUT STD_LOGIC_VECTOR (23 DOWNTO 0)
+        );
+    END COMPONENT;
+    COMPONENT shiftReg_16bit
+    PORT (
+		nRst: IN STD_LOGIC;
+		clk: IN STD_LOGIC;
+		sIN: IN STD_LOGIC;
+		Q: OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
+	);
+    END COMPONENT;
     COMPONENT control
         PORT (
             nGRst: IN  STD_LOGIC;
@@ -56,14 +68,19 @@ ARCHITECTURE structure OF Encoder IS
             add:   IN  STD_LOGIC_VECTOR (4 DOWNTO 0);
             nRst:  OUT STD_LOGIC;
             nSetO: OUT STD_LOGIC;
-            clkO:  OUT STD_LOGIC
+            clkO:  OUT STD_LOGIC;
+            clk1O: OUT STD_LOGIC
         );
     END COMPONENT;
     BEGIN
+        sr16: shiftReg_16bit PORT MAP(iNRst, clk1O, sIn, srO);
         pr9In <= bbO & sIn;
         pr9: parReg_9bit PORT MAP(iNRst, clk, pr9In, bbI);
 		bb1: BuildingBlock1 PORT MAP (bbI, bbO);
-		pr8: parReg_8bit PORT MAP(iNSet, clkO, bbO, dOut);
+        pr24In <= srO & bbO;
+        iOut <= srO;
+        clk1OO <= clk1O;
+		pr24: parReg_24bit PORT MAP(iNSet, clkO, pr24In, dOut);
 		bc:  binCounter_5bit PORT MAP (iNRst, clk, stat);
-		con: control  PORT MAP (nGRst, clk, stat, iNRst, iNSet, clkO);
+		con: control  PORT MAP (nGRst, clk, stat, iNRst, iNSet, clkO, clk1O);
 END structure;
